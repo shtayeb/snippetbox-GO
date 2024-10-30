c25b0eddce8550d414c0f51a4c34eed050586501
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-10-30 13:36:58 +0430

    templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,30 +22,30 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	// file in the slice
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
 	}
 
-	// file in the slice
-	// files := []string{
-	// 	"./ui/html/base.tmpl",
-	// 	"./ui/html/partials/nav.tmpl",
-	// 	"./ui/html/pages/home.tmpl",
-	// }
-	//
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	http.Error(w, "Internal Serve Error 1", 500)
-	//
-	// 	return
-	// }
-	//
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	http.Error(w, "Internal Serve Error 2", 500)
-	// }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		http.Error(w, "Internal Serve Error 1", 500)
+		return
+	}
+
+	// snippets
+	data := &templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+		http.Error(w, "Internal Serve Error 2", 500)
+	}
 
 }
 
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -5,5 +5,6 @@ import "github.com/shtayeb/snippetbox/internal/models"
 // Define a templateData type to act as the holding structure for
 // any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }

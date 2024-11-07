2986ce8b2b9dd292de7da621a5aacf33b53a000c
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-07 11:51:48 +0430

    user signup and password encrption

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,8 +2,11 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
 
+	"github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -25,9 +28,21 @@ func (m *UserModel) Insert(name, email, password string) error {
 		return err
 	}
 
-	stmt := `INSERT INTO users (name, email, hashed_password, created) VALUES(?, ?, ?, now())`
+	stmt := `INSERT INTO users (name, email, hashed_password, created) VALUES($1, $2, $3, now())`
+
 	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
 	if err != nil {
+		// If this returns an error, we use the errors.As() function to check whether the error has the type *mysql.MySQLError. If it does, the error will be assigned to the mySQLError variable.
+		// We can then check whether or not the error relates to our users_uc_email key by
+		// checking if the error code equals 1062 and the contents of the error
+		// message string. If it does, we return an ErrDuplicateEmail error.
+		var pgSQLError *pq.Error
+		if errors.As(err, &pgSQLError) {
+			// PostgreSQL unique constraint violation error code is "23505".
+			if pgSQLError.Code == "23505" && strings.Contains(pgSQLError.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
 		return err
 	}
 
